1972e368939e28f76759667cccb372a9bdd4e47f
Stan Dyro <stand@pk4media.com>
2013-10-28 13:58:01 -0700

    deprecating instance methods

--- lib/s3uploader.rb
+++ lib/s3uploader.rb
@@ -6,9 +6,7 @@ module S3uploader
   mattr_accessor :s3_key
   mattr_accessor :bucket
 
-  module InstanceMethods
-    def s3_signature(s3_policy)
-      Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'), self.s3_key, s3_policy)).gsub(/\n/, '')
-    end
+  def s3_signature(s3_policy)
+    Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'), self.s3_key, s3_policy)).gsub(/\n/, '')
   end
 end
