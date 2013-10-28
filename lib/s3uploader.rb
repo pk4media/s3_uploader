32bb32c2fb0256e82be3d4f8137d895d411096a8
Stan Dyro <stand@pk4media.com>
2013-10-28 12:45:42 -0700

    concerns

--- lib/s3uploader.rb
+++ lib/s3uploader.rb
@@ -1,12 +1,14 @@
 require "s3uploader/version"
 
 module S3uploader
+  extend ActiveSupport::Concern
 
   mattr_accessor :s3_key
   mattr_accessor :bucket
 
-  def s3_signature(s3_policy)
-    Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'), self.s3_key, s3_policy)).gsub(/\n/, '')
+  module InstanceMethods
+    def s3_signature(s3_policy)
+      Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'), self.s3_key, s3_policy)).gsub(/\n/, '')
+    end
   end
-
 end
