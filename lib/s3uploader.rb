4e9ce07d033ddf39377bafc0a9b4e5e0ab5b05ff
Stan Dyro <stand@pk4media.com>
2013-10-29 10:48:26 -0700

    added configuration

--- lib/s3uploader.rb
+++ lib/s3uploader.rb
@@ -1,47 +1,36 @@
 require "s3uploader/version"
 
-module S3uploader
+module S3Uploader
 
   extend ActiveSupport::Concern
 
-  mattr_accessor :s3_key
-  mattr_accessor :bucket
+  def s3_policy(key_starts_with, content_types: [], max_file_size: nil, acl: 'private', success_action_status: '201')
+    conditions = [
+      ["starts-with", "$utf8", ""],
+      ["starts-with", "$key", key_starts_with],
+      ["starts-with", "$x-requested-with", ""],
+      ["starts-with", "$content-type", config.content_type_starts_with || ""],
+      {bucket: config.bucket},
+      {acl: acl},
+      {success_action_status: success_action_status}
+    ]
 
-  @options = {
-  	expiration: 3.minutes.from_now.utc.xmlschema,
-  	key_starts_with: '',
-  	max_file_size: 786432000,
-    bucket: self.config.bucket,
-    acl: 'private',
-    success_action_status: '201'
-  }
-
-
-  def s3_signature(s3_policy)
-    Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'), self.s3_key, s3_policy)).gsub(/\n/, '')
-  end
-
-  def s3_policy(key: @options[:key_starts_with], content_types: [])
-
-		conditions = [
-			["starts-with", "$utf8", ""],
-			["starts-with", "$key", key],
-			["starts-with", "$x-requested-with", ""],
-			["content-length-range", 0, @options[:max_file_size]],
-			["starts-with","$content-type", @options[:content_type_starts_with] ||""],
-			{bucket: @options[:bucket]},
-			{acl: @options[:acl]},
-			{success_action_status: @options[:success_action_status]}
-		]
+    unless max_file_size.nil?
+      conditions << ["content-length-range", 0, max_file_size]
+    end
     
     content_types.each do |type|
       conditions << ["starts-with", "$Content-Type", type]
     end
     
     policy = {
-      expirations: options[:expiration],
+      expirations: @expiration.from_now.utc.xmlschema,
       conditions: conditions 
     }
+  end
+
+  def s3_signature(s3_policy)
+    Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'), config.access_key, s3_policy)).gsub(/\n/, '')
   end
 
 end
---
+++ lib/s3uploader/config.rb
@@ -0,0 +1,23 @@
+module S3Uploader
+  class << self
+    attr_accessor :config
+  end
+
+  def self.configure
+    self.config ||= Config.new
+    yield config
+  end
+
+  class Config
+    attr_accessor :access_key
+    attr_accessor :bucket
+
+    def initialize
+      @access_key = ENV['AWS_ACCESS_KEY']
+      @bucket = nil
+      @expiration = 3.minutes
+      @content_type_starts_with = 'jpg'
+      @success_action_status = '201'
+    end
+  end
+end
